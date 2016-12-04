c42fa2800bbf5bcd77985c5358711a7d403dae84
DESKTOP-360HOOV\linyi <linyiq88@outlook.com>
2016-12-04 21:39:31 +0800

    Testing2

--- Get_WindowsServices_Logon.ps1
+++ Get_WindowsServices_Logon.ps1
@@ -3,4 +3,5 @@
 Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
 #Run on different Servers
 #Get-WMIObject Win32_Service -ComputerName comname | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
+#
 
