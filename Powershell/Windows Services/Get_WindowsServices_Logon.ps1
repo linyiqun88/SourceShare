f6a4f15b137545a51b6f8b39495d914d2fd409d4
DESKTOP-360HOOV\linyi <linyiq88@outlook.com>
2016-12-04 22:07:41 +0800

    update: comment

--- Powershell/Windows Services/Get_WindowsServices_Logon.ps1
+++ Powershell/Windows Services/Get_WindowsServices_Logon.ps1
@@ -1,7 +1,10 @@
-﻿$serviceFilter = "LocalSystem"#, "NT AUTHORITY\NetworkService"
+﻿#Script version: 1.0
+#Script to be run on different servers to retrieve the service and there logons
+#Status: Complete
+#Output: Retrieving service account logon to the services
+
+$serviceFilter = "LocalSystem"#, "NT AUTHORITY\NetworkService"
 #Testing on local 
 Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
 #Run on different Servers
-#Get-WMIObject Win32_Service -ComputerName comname | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
-#
-#
+
