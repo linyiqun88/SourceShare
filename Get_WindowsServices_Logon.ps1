15d0e928ad4c438c2dae84894d46b6b60769d52d
Clement Lin <Clement Lin>
2016-12-04 21:18:55 +0800

    Get Windows Services with service logon

---
+++ Get_WindowsServices_Logon.ps1
@@ -0,0 +1,6 @@
+﻿$serviceFilter = "LocalSystem"#, "NT AUTHORITY\NetworkService"
+#Testing on local 
+Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
+#Run on different Servers
+#Get-WMIObject Win32_Service -ComputerName comname | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
+
