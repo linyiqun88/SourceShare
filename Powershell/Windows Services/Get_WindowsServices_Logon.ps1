24c0e42772989c9d02c636b0116af982ba8e5022
clement <linyiqun88@outlook.com>
2016-12-05 01:27:29 +0800

    Testing

--- Powershell/Windows Services/Get_WindowsServices_Logon.ps1
+++ Powershell/Windows Services/Get_WindowsServices_Logon.ps1
@@ -6,5 +6,5 @@
 $serviceFilter = "LocalSystem"#, "NT AUTHORITY\NetworkService"
 #Testing on local 
 Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
-#Run on different Servers
+#Run on different Servers.
 
