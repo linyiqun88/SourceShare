bfe2bcdb62adbb59066d9f42e6b2374c744b0bed
clement <linyiqun88@outlook.com>
2016-12-05 10:08:34 +0800

    Update: Fix logging remote output into text file

--- Powershell/Custom/Get_WindowsService_Logon_RemoteServers.ps1
+++ Powershell/Custom/Get_WindowsService_Logon_RemoteServers.ps1
@@ -4,17 +4,24 @@
 #Output: Logging for services retrieved from multiple servers are saved in the host server which is running the script
 
 #Servers to be defined
-$servers = "*h1","*h2", "DESKTOP-360HOOV"
+$servers = "servername"
 #Service account to filter
-$serviceFilter = "LocalSystem"
+#$serviceFilter = "local"
+#p3ris@P3dev.mindef.gov.sg
 #Path of the log files
 $logfile = "C:\test.txt"
 
 $out = foreach($server in $servers){
     invoke-command -cn $server -scriptblock {
         Param($server,$serviceFilter)
-        #"{0} :Begin DLL registration for {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"),$server
-        Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
+        $serviceFilter = "Local"
+        "{0} :Begin windows service checking for {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"),$server       
+        #Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | Format-Table name, startname, startmode, state
+        $list = Get-WMIObject Win32_Service | Where-Object{$serviceFilter.Contains($_.StartName)} | select name, startname, startmode, state #Format-List name, startname, startmode, state
+        foreach($item in $list)
+        {
+             "{0} ,Window service name {1}, Logon: {2}, Startmode: {3}, State: {4}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"),$item.name, $item.startname, $item.startmode, $item.state
+        }
     } -ArgumentList $server
 }
 
